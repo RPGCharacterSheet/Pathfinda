65ae486af99b58e4a5b481c429343973fc9145e1
Joel Gimbel <jgimbel@live.com>
2016-11-07 22:16:32 -0600

    adding mongo connection

---
+++ MongoModels/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Core;
+namespace MongoModels
+{
+    public class Database
+    {
+        public MongoClient client;
+        public IMongoDatabase db;
+        private static Database _instance;
+        public static Database Instance
+        {
+            get { return (_instance == null) ? _instance = new Database() : _instance; }
+        }
+
+        private Database()
+        {
+            var connectionString = "mongodb://jogimbel.ddns.net";
+            client = new MongoClient(connectionString);
+            db = client.GetDatabase("Pathfinda");
+
+        }
+    }
+}
---
+++ MongoModels/Feats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+namespace MongoModels
+{
+    class Feats
+    {
+        private static MongoModels.Database db;
+        private static IMongoCollection<BsonDocument> collection;
+        static Feats()
+        {
+            db = Database.Instance;
+            collection = db.db.GetCollection<BsonDocument>("feats");
+        }
+
+        public List<BsonDocument> Find(BsonDocument query)
+        {
+            return collection.Find(query) as List<BsonDocument>;
+        }
+
+        public List<BsonDocument> FindAlike(string name)
+        {
+            return Find(new BsonDocument { { "name", new BsonDocument { { "$regex", name }, { "$options", "i" } } } });
+        }
+    }
+}
