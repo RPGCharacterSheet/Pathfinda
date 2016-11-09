48f4cace78e28df6208f9a844d51daaf4e4d6b0a
Joel Gimbel <jgimbel@live.com>
2016-11-08 23:51:12 -0600

    Update to database finds and updates

--- MongoModels/MongoEntity.cs
+++ MongoModels/MongoEntity.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MongoDB.Bson;
 namespace MongoModels
 {
     public class MongoEntityBase
     {
-        public object _id { get; set; }
+        public ObjectId _id { get; set; }
 
     }
 }
