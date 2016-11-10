eae4ea1d8811712d0f2d03a923d7d8ffacf10c64
Joel Gimbel <jgimbel@live.com>
2016-11-09 21:51:35 -0600

    lets make get and put work again!

--- MongoModels/MongoEntity.cs
+++ MongoModels/MongoEntity.cs
@@ -4,71 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Driver;
 namespace MongoModels
 {
-    public abstract class MongoEntityBase { }
-    public abstract class MongoEntityBase<T> : MongoEntityBase where T : MongoEntityBase
+    public abstract class MongoEntityBase
     {
         public ObjectId _id { get; set; }
+    }
+    public abstract class MongoEntityBase<T> : MongoEntityBase where T : MongoEntityBase
+    {
 
         // T will be Character, or Feat, or whatever
-        public static T Get(string someParametersToFindItInTheDatabase)
+        public static T Get(ObjectId id)
         {
-            bool foundRecordInMongo = someParametersToFindItInTheDatabase == "whatever";
-            if (foundRecordInMongo)
-            {
-                // return the one from Mongo, not default(T)
-                return default(T);
+            var collection = Database.Instance.db.GetCollection<T>(typeof(T).Name);
+            return collection.Find(doc => doc._id == id).First();
 
-
-                //return collection.Find(doc => doc._id == dataInUniqueField).First();
-
-                //        //or this could work if you like linq as better
-                //        //return (from e in allOfEm.AsQueryable<t>()
-                //        // where e._id == dataInUniqueField
-                //        // select e).First();
-
-
-                //        //or if you really want to use the uniqueField
-                //        //return AllOfEm.find(BsonDocument{ { uniqueField, dataInUnqiueField } }).First();
-
-
-                //        //in the shell this kind of query looks like
-                //        //db[typeof(T)].find({ 
-                //        //  [uniqueField]: dataInUniqueField
-                //        //})
-
-                //        //mongo is based off of javascript
-                //        //and in js, 
-                //        //db['characters']
-                //        //and
-                //        //db.characters
-                //        //are the same get command.
-                //        //hence db[typeof(T)]
-                //        //Will be the same as
-                //        //db.Character.find()
-
-            }
-            else
-            {
-                // return a fresh one
-                return Activator.CreateInstance(typeof(T), true) as T;
-            }
         }
-
-        public void Put()
+        //overwrite the character currently stored in the database
+        public static Task<T> Put(T replacement)
         {
-            //        //In shell this is db[typeof(T)].update(query, objectToOverwrite)
-            //        //In c#
-
-            //        return collection
-            //            .FindOneAndReplace<T>(
-            //                filter: doc => doc._id == insertOrUpdate._id,
-            //                replacement: insertOrUpdate
-            //            );
+            var collection = Database.Instance.db.GetCollection<T>(typeof(T).Name);
+            return collection
+                .FindOneAndReplaceAsync<T>(
+                    filter: doc => doc._id == replacement._id,
+                    replacement: replacement
+                );
         }
 
         // don't let anybody make a Character, or Feat, or Spell. Force them to call Character.Get()
+        //What if they want to create a new character?
         protected MongoEntityBase()
         {
 
