66341586edbc3e7734b61b42e7d55186fecaca9c
Joel Gimbel <jgimbel@live.com>
2016-11-11 18:59:18 -1200

    Add REST API

---
+++ WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoModels.Models;
+using MongoDB.Bson;
+namespace WebAPI.Controllers
+{
+    public class CharacterController : Controller
+    {
+
+        // GET: Character/5d37a94d...
+        public ActionResult Index(string id)
+        {
+            return Json(CharacterClass.GetById(ObjectId(id));
+        }
+
+        // POST: Character/Create
+        [HttpGet]
+        [HttpPost]
+        public ActionResult Create(Character character=CharacterClass.Create())
+        {
+            return Json(CharacterClass.Put(character));
+        }
+
+        // GET||Post: Character/Edit
+        [HttpGet]
+        [HttpPost]
+        public ActionResult Edit(Character character)
+        {
+            return Json(Character.Put(character));
+        }
+
+        // GET: Character/Delete/5
+        [HttpGet]
+        [HttpPost]
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            return Json(CharacterClass.Delete(MongoId(id)));
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoModels.Models;
+using MongoDB.Bson;
+namespace WebAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index(string userName, string password)
+        {
+            User.UserModel user = MongoModels.Models.User.getUser(userName, password);
+            if(user == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            return Json(user);
+        }
+
+        // GET: User/Create
+        [HttpGet]
+        [HttpPost]
+        public ActionResult Create(string username, string password, string email="")
+        {
+            return Json(MongoModels.Models.User.newUser(username, password));
+        }
+        
+        // GET: User/Delete/5
+        //public ActionResult Delete(string id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: User/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
