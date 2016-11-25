6caa7acdfbf2f217d0c589fc02e9d9c10f2c7e1d
Joel Gimbel <jgimbel@live.com>
2016-11-25 10:22:29 -1200

    Check values for ObjectId

--- WebAPI/Global.asax.cs
+++ WebAPI/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
+using MongoDB.Bson;
+
 using System.Web.Mvc;
 using System.Web.Optimization;
 namespace WebAPI
@@ -18,6 +20,20 @@ namespace WebAPI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.Add(typeof(ObjectId), new ObjectIdModelBinder());
+        }
+    }
+
+    public class ObjectIdModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var result = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (result == null)
+            {
+                return ObjectId.Empty;
+            }
+            return ObjectId.Parse((string)result.ConvertTo(typeof(string)));
         }
     }
 }
